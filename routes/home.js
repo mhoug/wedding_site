94efcdc3f40ddfc191d5f53ddb1b6f9c994a9ca1
LMoore <moore.lyndall@gmail.com>
2017-07-25 19:33:30 -0400

    Update title

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: '#lovebyrds2017 #byrdsofafeather', url: req.params.url });
+  res.render(
+    'home', 
+    {
+      title: 'James Byrd and Teresa Parler Wedding! - #lovebyrds2017 #byrdsofafeather',
+      url: req.params.url
+    }
+  );
 });
 
 module.exports = router;
